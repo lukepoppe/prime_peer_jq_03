162ec0c7388823b0ea7f4bb017a62a421dae0511
lukepoppe <lukepoppe@hotmail.com>
2015-04-27 17:11:42 -0500

    first commit

--- script.js
+++ script.js
@@ -3,13 +3,60 @@ var apikey = 'YOUR-API-KEY'; // Put your API key here
 // Use this function to do stuff with your results. 
 // It is called after 'search' is executed.
 function searchCallback(results) {
-    console.log(results);
+	for (var i = 0; i < 10; i++) {
+		var platformName = "";
+		for ( j = 0; j < results[i].platforms.length; j++) {
+			platformName +=  "- " + results[i].platforms[j].name;
+		};
+		
+		if((i +1) % 3 == 0 ){
+			$('#searchResults').append(
+				'<div id="searchRow" class= "row">' +
+					'<div id="result' + (i+1) + '" class="col-md-4">' +
+						'<div id="name"> Tittle: ' + results[i].name + '</div>' +
+						'<div id="image"><img src="' + results[i].image.thumb_url +'"/></div>' +
+						'<div id="description"><h4>Description: </h4>' + results[i].deck + '</div>' +
+						'<div id="platforms"><h4> Supported Platforms</h4>' + platformName + '</div>' +
+					'</div>' +
+				'</div>'
+			);
+
+		} else {
+		$('#searchResults').append(
+			'<div id="result' + (i+1) + '" class="col-md-4">' +
+				'<div id="name"> Tittle: ' + results[i].name + '</div>' +
+				'<div id="image"><img src="' + results[i].image.thumb_url +'"/></div>' +
+				'<div id="description"><h4>Description: </h4>' + results[i].deck + '</div>' +
+				'<div id="platforms"><h4> Supported Platforms</h4>' + platformName + '</div>' +
+			'</div>' 
+
+			);
+
+		};
+
+	}
 }
 
-$(document).ready(function() {
 
+
+
+
+var userInput = "";
+var apikey = "360640851fd82bba03cfed3e035021eb9eef3ef3";
+
+
+$(document).ready(function() {
+	$('#submit').on('click', function(){
+	$('#searchResults').empty();
+		console.log("before " + userInput);
+		userInput = $('#search').val();
+		console.log("after " + userInput);
+		search(userInput);
+	});
 	// Start the search here!
-	search('batman');
+	
+
+
 });
 
 // HELPER FUNCTION
@@ -21,12 +68,13 @@ function search(query){
 	    dataType: 'jsonp',
 	    crossDomain: true,
 	    jsonp: 'json_callback',
-	    url: 'http://www.giantbomb.com/api/search/?format=jsonp&api_key=' + apikey +'&query=' + encodeURI(query),
+	    url: 'http://www.giantbomb.com/api/search/?format=jsonp&resources=game&api_key=' + apikey +'&query=' + encodeURI(query),
 	    complete: function() {
 	        console.log('ajax complete');
 	    },
 	    success: function(data) {
 	        searchCallback(data.results);
+	        console.log("results :" + data.results);
 	    }
 	});
 
